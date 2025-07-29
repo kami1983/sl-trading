351c5e4d8e1c152f3d688cb3ed0210244713a7aa
kami1983 <kami@cancanyou.com>
2025-07-29 13:21:08 +0800

    可以提交的版本

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next'
+import path from 'path'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@project/anchor': path.resolve(__dirname, './anchor/src'),
+    }
+    return config
+  },
 }
 
 export default nextConfig
